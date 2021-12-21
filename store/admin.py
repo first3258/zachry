874dec017a8911901486738268fae5d272a96f5c
sekyelent <54430426+sekyelent@users.noreply.github.com>
2021-12-21 13:16:48 +0700

    new bookstore

---
+++ store/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from . import models
+from store.models import Category,Product, Cart, CartItem, Order, OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id', 'name', 'total', 'token', 'created', 'updated', 'status']
+    list_editable = ['status']
+    
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=['order', 'product', 'quantity', 'price', 'created', 'updated']
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
---
+++ store/static/css/account.js
@@ -0,0 +1,7 @@
+function openEdit() {
+    document.getElementById("edit").style.display = "flex";
+}
+  
+function closeEdit() {
+    document.getElementById("edit").style.display = "none";
+}
